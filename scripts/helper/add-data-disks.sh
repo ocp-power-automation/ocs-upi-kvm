b74afcda43353cba750d15d622a3131e48190839
Luke Browning <lukebrowning@us.ibm.com>
2020-09-16 17:15:34 -0500

    Turn off bash trace for grow data & boot disks
    Add print statements to show high level flow.
    
    Allow boot volume to be resized when the VM is shutdown.
    
    Signed-off-by: Luke Browning <lukebrowning@us.ibm.com>

--- scripts/helper/add-data-disks.sh
+++ scripts/helper/add-data-disks.sh
@@ -2,7 +2,7 @@
 
 # Create and attach a data disk to each worker node
 
-set -xe
+set -e
 
 if [ ! -e helper/parameters.sh ]; then
         echo "Please invoke this script from the directory ocs-upi-kvm/scripts"
@@ -19,7 +19,12 @@ rm -f $IMAGES_PATH/test-ocp$OCP_VERSION/*.data
 
 for (( i=0; i<$WORKERS; i++ ))
 do
+	echo "Creating data disk $IMAGES_PATH/test-ocp$OCP_VERSION/disk-worker${i}.data ${DATA_DISK_SIZE}G"
+
 	qemu-img create -f raw $IMAGES_PATH/test-ocp$OCP_VERSION/disk-worker${i}.data ${DATA_DISK_SIZE}G
- 	virsh list | grep worker-$i | tail -n +1 | awk -v var="$i" '{system("virsh attach-disk " $2 " --source $IMAGES_PATH/test-ocp$OCP_VERSION/disk-worker" var ".data --target vdc --persistent")}'
+
+	echo "Attaching data disk to test-ocp$OCP_VERSION-worker-$i"
+
+ 	virsh list | grep worker-$i | tail -n 1 | awk -v var="$i" '{system("virsh attach-disk " $2 " --source $IMAGES_PATH/test-ocp$OCP_VERSION/disk-worker" var ".data --target vdc --persistent")}'
 done
 
