fc6015f4705f725bc7d19cf6fe5df927bd5fc923
Luke Browning <lukebrowning@us.ibm.com>
2020-09-17 11:17:55 -0500

    Add capability to use logical disk partitions for data disks
    A new environment variable is defined for this purpose.  It should
    be set to the list of disk partitions to be used.  A unique disk must
    be specified for each worker node.  For example,
    
    export DATA_DISK_LIST=sdc1,sdd1,sde1
    
    This example assumes 3 worker nodes.
    
    If the DATA_DISK_LIST is not set, then qcow2 images are allocated
    by the scripts.
    
    Signed-off-by: Luke Browning <lukebrowning@us.ibm.com>

--- scripts/helper/add-data-disks.sh
+++ scripts/helper/add-data-disks.sh
@@ -2,6 +2,10 @@
 
 # Create and attach a data disk to each worker node
 
+# One can attach a disk partition or a qcow2 file.  The qcow2 method is
+# the default, which can be overridden by specifying a comma separated list
+# of disk partitions via the environment variable DATA_DISK_LIST.
+
 set -e
 
 if [ ! -e helper/parameters.sh ]; then
@@ -11,11 +15,28 @@ fi
 
 source helper/parameters.sh
 
+if [ -n "$DATA_DISK_ARRAY" ]; then
+
+	for (( i=0; i<$WORKERS; i++ ))
+	do
+		pname=${DATA_DISK_ARRAY[$i]}
+		echo "Attaching /dev/$pname to test-ocp$OCP_VERSION-worker-$i"
+ 		virsh list | grep worker-$i | tail -n 1 | awk -v var="$i" '{system("virsh attach-disk " $2 " --source /dev/$pname --target vdc --persistent")}'
+	done
+
+	exit 
+fi
+
 # Remember where files were created for virsh_cleanup.sh
 
 echo "$IMAGES_PATH" > ~/.images_path
 
 rm -f $IMAGES_PATH/test-ocp$OCP_VERSION/*.data
+
+if [ "$DATA_DISK_SIZE" -le "0" ]; then
+	echo "No data disks will be created"
+	exit
+fi
 
 for (( i=0; i<$WORKERS; i++ ))
 do
--- scripts/helper/parameters.sh
+++ scripts/helper/parameters.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-set +x
-export RHID_PASSWORD=${RHID_PASSWORD:=''}
-set -x
+export RHID_PASSWORD=${RHID_PASSWORD:=""}
 
 export RHID_USERNAME=${RHID_USERNAME:=""}
 
@@ -18,6 +16,8 @@ export WORKERS=${WORKERS:=2}
 export WORKER_DESIRED_CPU=${WORKER_DESIRED_CPU:="4"}
 export WORKER_DESIRED_MEM=${WORKER_DESIRED_MEM:="16384"}
 
+export DATA_DISK_LIST=${DATA_DISK_LIST:=""}		# in GBs
+
 export DATA_DISK_SIZE=${DATA_DISK_SIZE:=100}		# in GBs
 export BOOT_DISK_SIZE=${BOOT_DISK_SIZE:=32}		# in GBs
 
@@ -28,3 +28,43 @@ export IMAGES_PATH=${IMAGES_PATH:="/var/lib/libvirt/images"}
 # This image is obtained from RedHat Customer Portal and then prepared for use
 
 export BASTION_IMAGE=${BASTION_IMAGE:="rhel-8.2-update-2-ppc64le-kvm.qcow2"}
+
+
+# Validate DATA_DISK_LIST upfront so that mistakes are captured early 
+
+if [ -n "$DATA_DISK_LIST" ]; then
+
+	if [[ "$DATA_DISK_LIST" =~ "/" ]]; then
+		echo "Error: just specify the disk partition name --- sdi1,sdi2,sdi3"
+		exit 1
+	fi
+
+	# Convert comma separated list into an array of partition names
+
+	list=${DATA_DISK_LIST//,/ }
+	DATA_DISK_ARRAY=($list)
+	n=${#DATA_DISK_ARRAY[@]}
+
+	if [ "$n" != "$WORKERS" ]; then
+		echo "Error: a single disk partition should be specified per worker node"
+		exit 1
+	fi
+
+	unique_disks=$(echo $list | xargs -n 1 | sort -u | xargs)
+	unique_disk_array=($unique_disks)
+	u=${#unique_disk_array[@]}
+
+	if [ "$n" != "$u" ]; then
+		echo "Error: invalid disk partition list $DATA_DISK_LIST.  A unique disk partition must be specified per worker node"
+		exit 1
+	fi
+
+	available_disks=$(lsblk -a)
+	for i in $list
+	do
+		if [[ "$available_disks" != *"$i"* ]]; then 
+			echo "Error: invalid disk partition list $DATA_DISK_LIST.  lsblk -a does not show partition $i"
+			exit 1
+		fi
+	done
+fi
