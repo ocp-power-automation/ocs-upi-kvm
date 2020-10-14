63bdaade8d282c6a176c3f0af7433beebb665f84
Luke Browning <lukebrowning@us.ibm.com>
2020-10-14 14:58:48 -0500

    Use python3.6 for ocs-ci instead of python3.8
    python3.6 is the default for rhel 8.2 and is most commonly
    used for ocs-ci by the Redhat ocs-ci team.
    
    Signed-off-by: Luke Browning <lukebrowning@us.ibm.com>

--- scripts/setup-ocs-ci.sh
+++ scripts/setup-ocs-ci.sh
@@ -7,8 +7,8 @@ if [ ! -e helper/parameters.sh ]; then
 	exit 1
 fi
 
-sudo yum -y install libffi-devel lapack atlas-devel gcc-gfortran openssl-devel gcc-gfortran
-sudo yum -y install python38-devel python38-setuptools python38-Cython python3-virtualenv python3-docutils
+sudo yum -y install libffi-devel lapack atlas-devel openssl-devel gcc gcc-c++ gcc-gfortran make
+sudo yum -y install python36-devel python3-setuptools python3-virtualenv python3-docutils
 
 source helper/parameters.sh
 
@@ -16,13 +16,12 @@ pushd ../src/ocs-ci
 
 rm -rf $WORKSPACE/venv
 
-python3.8 -m venv $WORKSPACE/venv
+python3.6 -m venv $WORKSPACE/venv
 
 source $WORKSPACE/venv/bin/activate		# activate named python venv
 
-pip install --upgrade pip setuptools
-pip install wheel
-pip install -r requirements.txt
+pip3 install --upgrade pip setuptools wheel
+pip3 install -r requirements.txt
 
 deactivate					# exit venv shell
 
