65520efb62a2e5ecb0ee5641a087e5f55ba44c55
Luke Browning <lukebrowning@us.ibm.com>
2021-01-14 12:22:05 -0600

    Fix greenlet python module problem by reverting previous version
    Greenlet 1.0 was released Jan 13th
    
    Signed-off-by: Luke Browning <lukebrowning@us.ibm.com>

--- scripts/deploy-ocs-ci.sh
+++ scripts/deploy-ocs-ci.sh
@@ -24,43 +24,6 @@ export KUBECONFIG=$WORKSPACE/auth/kubeconfig
 
 pushd ../src/ocs-ci
 
-patchfiles=(../../files/ocs-ci/ocs-ci-[0-9][0-9]-*.patch)
-
-kvm_present=$(/usr/sbin/lsmod | grep kvm)
-if [ -n "$kvm_present" ]; then
-	platform=kvm
-else
-	platform=powervs
-fi
-
-platform_patchfiles=(../../files/ocs-ci/$platform/ocs-ci-*[0-9][0-9]-*.patch)
-
-# Patch OCS-CI if a patch is available
-
-if [[ "${#patchfiles[@]}" -gt 0 ]] || [[ "${#platform_patchfiles[@]}" -gt 0 ]]; then
-
-	echo "Generating consolidated patch file $WORKSPACE/ocs-ci.patch from $WORKSPACE/ocs-upi-kvm/files/ocs-ci/"
-	> $WORKSPACE/ocs-ci.patch
-	if [[ "${#patchfiles[@]}" -gt 0 ]]; then
-		cat "${patchfiles[@]}" >> $WORKSPACE/ocs-ci.patch
-	fi
-	if [[ "${#platform_patchfiles[@]}" -gt 0 ]]; then
-		cat "${platform_patchfiles[@]}" >> $WORKSPACE/ocs-ci.patch
-	fi
-
-	set +e
-	patch --dry-run -p1 < $WORKSPACE/ocs-ci.patch
-	rc=$?
-	set -e
-
-	if [ "$rc" == "0" ]; then
-		echo "Patching ocs-ci..."
-		patch -p1 < $WORKSPACE/ocs-ci.patch
-	else
-		echo "WARNING: Failed to patch ocs-ci.  Has git submodule ocs-ci HEAD changed?"
-	fi
-fi
-
 # WORKAROUND for ocs-ci bug that downloads x86 binary
 
 cp -f $WORKSPACE/bin/oc bin
@@ -83,6 +46,8 @@ yq -y -i '.RUN.log_dir |= env.LOGDIR' $WORKSPACE/ocs-ci-conf.yaml
 if [ "$platform" == "powervs" ]; then
 	yq -y -i '.ENV_DATA.number_of_storage_disks = 8' $WORKSPACE/ocs-ci-conf.yaml
 fi
+
+echo "run-ci -m deployment --deploy -ocs-version $OCS_VERSION ..."
 
 run-ci -m deployment --deploy \
 	--ocs-version $OCS_VERSION --cluster-name ocstest \
--- scripts/setup-ocs-ci.sh
+++ scripts/setup-ocs-ci.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 if [ ! -e helper/parameters.sh ]; then
 	echo "This script should be invoked from the directory ocs-upi-kvm/scripts"
 	exit 1
@@ -13,6 +11,40 @@ sudo yum -y install python36-devel python3-setuptools python3-virtualenv python3
 source helper/parameters.sh
 
 pushd ../src/ocs-ci
+
+patchfiles=( $(ls ../../files/ocs-ci/ocs-ci-[0-9][0-9]-*.patch) )
+echo "patchfiles=${patchfiles[@]}"
+
+platform_patchfiles=( $(ls ../../files/ocs-ci/$PLATFORM/ocs-ci-*[0-9][0-9]-*.patch) )
+echo platform_patchfiles=${platform_patchfiles[@]}
+
+set -e
+
+# Patch OCS-CI if a patch is available
+
+if [[ "${#patchfiles[@]}" -gt 0 ]] || [[ "${#platform_patchfiles[@]}" -gt 0 ]]; then
+
+        echo "Generating consolidated patch file $WORKSPACE/ocs-ci.patch from $WORKSPACE/ocs-upi-kvm/files/ocs-ci/"
+        > $WORKSPACE/ocs-ci.patch
+        if [[ "${#patchfiles[@]}" -gt 0 ]]; then
+                cat "${patchfiles[@]}" >> $WORKSPACE/ocs-ci.patch
+        fi
+        if [[ "${#platform_patchfiles[@]}" -gt 0 ]]; then
+                cat "${platform_patchfiles[@]}" >> $WORKSPACE/ocs-ci.patch
+        fi
+
+        set +e
+        patch --dry-run -p1 < $WORKSPACE/ocs-ci.patch
+        rc=$?
+        set -e
+
+        if [ "$rc" == "0" ]; then
+                echo "Patching ocs-ci..."
+                patch -p1 < $WORKSPACE/ocs-ci.patch
+        else
+                echo "WARNING: Failed to patch ocs-ci.  Has git submodule ocs-ci HEAD changed?"
+        fi
+fi
 
 rm -rf $WORKSPACE/venv
 
