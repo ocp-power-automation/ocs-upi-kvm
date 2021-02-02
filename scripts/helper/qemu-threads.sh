0538f939d95b4d6f0c19f5648f3f6612cd65c9ac
Luke Browning <lukebrowning@us.ibm.com>
2021-02-02 07:53:18 -0600

    Improve qemu-thread script for performance tuning
    Signed-off-by: Luke Browning <lukebrowning@us.ibm.com>

--- scripts/helper/qemu-threads.sh
+++ scripts/helper/qemu-threads.sh
@@ -20,3 +20,29 @@ type numastat > /dev/null 2>&1
 if [ "$?" == 0 ]; then
 	sudo numastat -c qemu
 fi
+
+echo -e "\nhost kvm server"
+ssh root@192.168.88.2 uptime
+ssh root@192.168.88.2 vmstat -w
+
+echo -e "\nbastion-$j"
+ssh root@192.168.88.2 uptime
+ssh root@192.168.88.2 vmstat -w
+
+j=0
+for i in 192.168.88.4 192.168.88.5 192.168.88.6
+do
+	echo -e "\nmaster-$j"
+	ssh -o StrictHostKeyChecking=no core@$i uptime
+	ssh -o StrictHostKeyChecking=no core@$i vmstat -w
+	((j++))
+done
+
+j=0
+for i in 192.168.88.21 192.168.88.22 192.168.88.23
+do
+	echo -e "\nworker-$j"
+	ssh -o StrictHostKeyChecking=no core@$i uptime
+	ssh -o StrictHostKeyChecking=no core@$i vmstat -w
+	((j++))
+done
