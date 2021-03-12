bad59c3f721582c83dacee4f5d1d74c27dc5ecb7
Luke Browning <lukebrowning@us.ibm.com>
2021-03-11 18:35:41 -0600

    Enable support for OCP 4.7 cluster logging
    Signed-off-by: Luke Browning <lukebrowning@us.ibm.com>

--- scripts/deploy-ocp-logging.sh
+++ scripts/deploy-ocp-logging.sh
@@ -9,14 +9,25 @@ fi
 
 source helper/parameters.sh
 
-export KUBECONFIG=$WORKSPACE/auth/kubeconfig
+if [ ! -e $WORKSPACE/env-ocp.sh ]; then
+	echo "The OpenShift Cluster must be created before deploying cluster logging"
+	exit 1
+fi
+
+source $WORKSPACE/env-ocp.sh
+
+oc get namespace/openshift-storage > /dev/null 2>&1
+if [ "$?" != 0 ]; then
+	echo "OpenShift Container Storage must be deployed before deploying cluster logging"
+	exit 1
+fi
 
 mkdir -p $WORKSPACE/deploy-ocp-logging
 pushd $WORKSPACE/deploy-ocp-logging
 
 # Copy clusterlogging configuration files to workspace as they are modified
 
-cp -f $WORKSPACE/ocs-upi-kvm/files/logging/* .
+cp -f $WORKSPACE/ocs-upi-kvm/files/cluster-logging/* .
 
 echo "Creating namespaces for openshift-operators-redhat and openshift-logging"
 
@@ -25,15 +36,28 @@ oc create -f cl-namespace.yaml
 
 echo "Creating elasticsearch operator group and subscriptions"
 
+case "$OCP_VERSION" in
+	4.4|4.5|4.6)
+		CHANNEL=$OCP_VERSION
+		;;
+	4.7|4.8)
+		CHANNEL=stable
+		;;
+	*)
+		echo "Unsupported OCP Version $OCP_VERSION"
+		exit 1
+		;;
+esac
+
 oc create -f eo-og.yaml
-oc create -f eo-rbac.yaml 					# rbac is specific to ocs-ci
-sed -i "s/VERSION_PLACEHOLDER/$OCP_VERSION/" eo-sub.yaml
+#oc create -f eo-rbac.yaml 				# rbac is specific to ocs-ci
+sed -i "s/VERSION_PLACEHOLDER/$CHANNEL/" eo-sub.yaml
 oc create -f eo-sub.yaml
 
 echo "Creating cluster-logging operator group and subscriptions"
 
 oc create -f cl-og.yaml
-sed -i "s/VERSION_PLACEHOLDER/$OCP_VERSION/" cl-sub.yaml
+sed -i "s/VERSION_PLACEHOLDER/$CHANNEL/" cl-sub.yaml
 oc create -f cl-sub.yaml
 
 sleep 1m
@@ -44,7 +68,7 @@ oc get csv --all-namespaces | egrep 'NAMESPACE|elasticsearch|openshift-logging'
 
 echo "Creating cluster-logging instance"
 
-oc create -f instance.yaml
+oc create -f cl-instance.yaml
 
 sleep 2m
 
