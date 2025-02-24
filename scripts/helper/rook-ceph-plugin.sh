09e07277ea4edd724c9f413eb056ea53ac0a5aaa
Aaruni Aggarwal <aaruniagg@gmail.com>
2025-02-24 06:50:28 -0500

    Adding script for installing kubectl-rook-ceph plugin

---
+++ scripts/helper/rook-ceph-plugin.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+
+if [ -e /usr/local/go/bin/go ]; then
+      echo "Go binary is already installed, hence exporting the path."
+      export PATH=/usr/local/go/bin:$PATH
+else
+      echo "Go binary not found. Installing Go..."
+      ARCH=`arch`
+      [ "$ARCH" == "x86_64" ] && ARCH="amd64"
+      VERSION="${GO_VERSION:-$(curl -s https://go.dev/dl/?mode=json | jq -r '.[0].version')}"
+      wget https://golang.org/dl/"${VERSION}".linux-"${ARCH}".tar.gz
+      rm -rf /usr/local/go && tar -C /usr/local -xvzf "${VERSION}".linux-"${ARCH}".tar.gz
+      export PATH=/usr/local/go/bin:$PATH
+fi
+
+echo -e "\n Cloning kubectl-rook-ceph repository...\n"
+
+git clone https://github.com/rook/kubectl-rook-ceph.git
+cd kubectl-rook-ceph/ && make build
+./bin/kubectl-rook-ceph --help
+
+echo -e "\n Kubectl Rook Ceph Plugin installed Successfully"
+
+oc get namespace/openshift-storage > /dev/null 2>&1
+if [ "$?" == 0 ]; then
+	./bin/kubectl-rook-ceph -n openshift-storage rook version
+	exit 1
+fi
